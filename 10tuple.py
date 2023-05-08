6cc176477c801dc9c60e943c35d9b4f6a17665be
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-09 02:12:02 +0300

    commit

---
+++ 10tuple.py
@@ -0,0 +1,21 @@
+list= [1,2,3]
+
+tuple=(1,'iki', 3)
+
+# print(type(list))
+# print(type(tuple))
+
+# print(list[2])
+# print(tuple[2])
+
+# print(len(list))
+# print(len(tuple))
+
+list=["ali", "veli"]
+tuple=("damla","ayşe","ayşe")
+names=("demet","emel","ayşe")+ tuple
+print(names)
+list[0]="ahmet"
+# tuple[0]="deniz" 
+print(tuple.count("ayşe"))
+print(tuple.index("ayşe"))
