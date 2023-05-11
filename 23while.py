363b7f26ae18a54bbb47d1c77f27721f37f0e615
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-11 23:50:11 +0300

    commit

---
+++ 23while.py
@@ -0,0 +1,35 @@
+# 1-100 e kadar
+# x=0
+# while x<100:
+#     if x==0:
+#         print("Başlıyorum...")
+#         x+=1
+#     else:
+#         print(x)
+#         x+=1
+# print("100!! Önüm Arkam Sağım Solum Sobe! Saklanmayan Ebe!")    
+# x=1
+# while x<=100:
+#     if x%2==0:
+#         print(x)
+#     x+=1
+# print("Çift Sayılar bitti")
+# x=1
+# while x<=100:
+#     if x%2==1:
+#         print(x)
+#     x+=1
+# print("Tek Sayılar bitti")
+x=1
+# while x<=100:
+#     if x%2==1:
+#         print(f"Sayı tek: {x}")
+#     else:
+#         print(f"Sayı çift: {x}")
+#     x+=1
+# print("Sayılar bitti")
+
+name="" #False
+while not name.strip():
+    name= input("İsminizi giriniz: ")
+print(f"Merhaba, {name}")
