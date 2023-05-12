f1e1248472992b9ba07ff18756325e929205e9a2
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-12 04:53:33 +0300

    commit

---
+++ 29methods.py
@@ -0,0 +1,13 @@
+# method
+
+list= [1,2,3]
+list.append(4)#append bir method
+print(list)
+list.pop()#pop bir method
+print(type(list))#type bir method
+print(list)
+
+myString= "Hello"
+print(type(myString))
+print(myString.upper())
+
