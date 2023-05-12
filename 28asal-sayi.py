d256bdb4a6b8946f59af2628d7d29125266c5c94
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-12 04:12:23 +0300

    commit

---
+++ 28asal-sayi.py
@@ -0,0 +1,13 @@
+# Girilen bir sayının asal olup olmadığını bulun
+a=float(input("Sayı girin: "))
+to=(a**0.5)+1
+if a<=1: print(f"{int(a)} sayısı asal değildir.")
+    
+else:
+    asalMi= True
+    for x in range(2,int(to)):
+        if a%x==0:
+            asalMi=False 
+            break
+    if asalMi:print(f"{int(a)} sayısı asaldır.")
+    else:print(f"{int(a)} sayısı asal değildir.")
