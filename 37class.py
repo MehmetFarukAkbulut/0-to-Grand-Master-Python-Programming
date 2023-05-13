ffc5fa9583fb11ccf42c55446dd777fe4e4e16eb
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-13 18:42:35 +0300

    commit

--- 37class.py
+++ 37class.py
@@ -6,8 +6,7 @@ class Person:
     def __init__(self,name,year):
         # object attributes
         self.name= name
-        self.birthyear= year
-        print("init methodu çalıştı")
+        self.year= year
     # methods
 
 # if a>10:
@@ -19,15 +18,15 @@ p2=Person(name="Fatih",year=1991)
 
 # updating
 p1.name="Rümeysa"
-p1.birthyear= 1998
+p1.year= 1998
 p1.address="İstanbul"
 p2.name="Faruk"
-p2.birthyear= 2000
+p2.year= 2000
 p2.address="İstanbul"
 
 # accesing object attributes
-print(f"name: {p1.name} year: {p1.birthyear} address: {p1.address}")
-print(f"name: {p2.name} year: {p2.birthyear} address: {p2.address}")
+print(f"name: {p1.name} year: {p1.year} address: {p1.address}")
+print(f"name: {p2.name} year: {p2.year} address: {p2.address}")
 print(p1)
 print(p2)
 print(type(p1))
