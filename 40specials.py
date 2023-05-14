841dc962027e529d5b42551582c5b33e97786c93
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-15 00:24:04 +0300

    commit

---
+++ 40specials.py
@@ -0,0 +1,16 @@
+mylist=[1,2,3]
+myString="my string"
+
+print(len(mylist))
+print(len(myString))
+print(type(mylist))
+print(type(myString))
+
+class Movie():
+    pass
+m=Movie()
+
+print(type(m))
+print(len(m))
+
+
