1a28acae9b61e5db9d409f66ec18ed0e0d57be9a
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-14 03:41:41 +0300

    commit

--- 39inheritance.py
+++ 39inheritance.py
@@ -18,16 +18,22 @@ class Person():
     
     
 class Student(Person):
-    def __init__(self,fname,lname):
+    def __init__(self,fname,lname,number):
         Person.__init__(self,fname,lname)
+        self.studentNumber=number
         print("Student Created.")
-
+    # override
+    def who_am_I(self):
+        print("I am a student")
+    def sayHello(self):
+        print("Hello I am a Student.")
 p1=Person("Faruk","Akbulut")
-s1=Student("Rümeysa","Akbulut")
+s1=Student("Rümeysa","Akbulut", 1434)
 print(p1.firstName+" "+p1.lastName)
-print(s1.firstName+" "+s1.lastName)
+print(f"{s1.firstName} {s1.lastName} {s1.studentNumber} ")
 
 p1.who_am_I()
 s1.who_am_I()
 p1.eat()
 s1.eat()
+s1.sayHello()
