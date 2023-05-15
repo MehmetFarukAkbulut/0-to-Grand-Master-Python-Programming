60fb8c794936c3306714b759646f247db4127548
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-16 01:27:50 +0300

    commit

---
+++ 42math.py
@@ -0,0 +1,35 @@
+# Yöntem 1
+# import math
+# import math as islem
+
+
+
+
+# value=dir(math)
+# value=help(math)
+# value=help(math.factorial)
+# value=math.sqrt(49)
+# value=math.factorial(5)
+# value=math.floor(5.9)
+# value=math.ceil(5.9)
+# value=islem.factorial(5)
+
+# Yöntem 2
+# from math import *
+
+from math import factorial,sqrt,ceil
+
+
+def sqrt(x):
+    print("x: "+str(x))
+
+
+
+# value=factorial(5)
+value=sqrt(9)
+# value=ceil(9.8)
+
+
+
+print(value)
+
