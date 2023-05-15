17fd7a9613986bf10589b494c55156bfedafe0ff
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-15 22:38:13 +0300

    commit

--- 40specials.py
+++ 40specials.py
@@ -13,7 +13,24 @@ class Movie():
         self.director=director
         self.duration=duration
         print("movie objesi oluşturuldu.")
-m=Movie("film adı","yönetmen adı", "filmin süresi")
-print(str(mylist))
+        
+    def __str__(self):
+        return f"{self.title} by {self.director}"
+    
+    
+    def __len__(self):
+        return self.duration
+    def __del__(self):
+        print("film objesi silindi")
+    
+    
+m=Movie("film adı","yönetmen adı", 120)
+# print(str(mylist))
 print(str(m))
+# print(len(mylist))
+# print(len(m))
 
+del m
+
+
+# print(m)
