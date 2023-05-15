b320e6b66e05dfdc0c6e8d9e6d88b62fe0ad374d
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-16 02:44:00 +0300

    commit

---
+++ 44mod.py
@@ -0,0 +1,27 @@
+'''
+    modül hakkında bilgilendirme
+'''
+print("Modül eklendi...")
+
+number=10
+
+numbers=[1,2,3]
+
+person={
+    "name":"Faruk",
+    "age":"23",
+    "city":"istanbul",
+
+}
+
+def func(x):
+    '''
+        fonksiyon hakkında bilgilendirme
+    '''
+    print(f"x: {x}")
+    
+    
+    
+class Person:
+    def speak(self):
+        print("I am speaking...")
