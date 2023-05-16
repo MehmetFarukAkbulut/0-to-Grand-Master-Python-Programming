182009445327592ab37e4859d69f6d6ec3468544
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-16 15:55:30 +0300

    commit

--- main45.py
+++ main45.py
@@ -1,1 +1,15 @@
 import mod44
+
+
+# result =help(mod44)
+# result =help(mod44.func)
+result=mod44.number
+result=mod44.numbers
+result=mod44.person["name"]
+result=mod44.person["age"]
+result=mod44.func(10)
+p= mod44.Person()
+p.speak()
+
+
+print(result)
