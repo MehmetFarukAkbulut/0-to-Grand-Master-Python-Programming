00c1686d662c5b2bb489c9d4a77360d79da0dab5
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-19 16:41:34 +0300

    commit

---
+++ 66_requests.py
@@ -0,0 +1,17 @@
+# requests yüklemek için 
+# pip install requests
+
+import requests
+import json
+
+result=requests.get("https://jsonplaceholder.typicode.com/todos")
+result=json.loads(result.text)
+
+for i in result:
+    if i["userId"]==1:
+        print(i["title"])
+        
+
+
+
+print(type(result))
