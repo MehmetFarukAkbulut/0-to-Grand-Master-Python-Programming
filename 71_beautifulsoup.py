a863ee428b57e35cec221efef464a70c137c7981
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-20 04:06:15 +0300

    commit

---
+++ 71_beautifulsoup.py
@@ -0,0 +1,104 @@
+html_doc='''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Web Sayfam</title>
+</head>
+<body>
+
+
+<h1 id="header">
+        Python Kursu
+    </h1>
+
+<div class="grup1">
+        <h2>
+            Programlama
+</h2>
+<ul>
+            <li>Menü 1</li>
+<li>Menü 2</li>
+<li>Menü 3</li>
+
+        </ul>
+    </div>
+
+    <div class="grup2">
+        <h2>
+            Modüller
+        </h2>
+
+        <ul>
+            <li>Menü 1</li>
+            <li>Menü 2</li>
+            <li>Menü 3</li>
+
+        </ul>
+    </div>
+    <div class="grup3">
+        <h2>
+            Django
+        </h2>
+
+        <ul>
+            <li>Menü 1</li>
+            <li>Menü 2</li>
+            <li>Menü 3</li>
+
+        </ul>
+    </div>
+
+    <img src="Programing in Pikachu.jpeg">
+    
+    <a class="sister" href="http://example1.com/elsie" id="Link1">Elsie</a> 
+    <a class="sister" href="http://example2.com/elsie" id="Link1">Elsie</a> 
+    <a class="sister" href="http://example3.com/elsie" id="Link1">Elsie</a> 
+    
+    
+</body>
+</html>
+'''
+from bs4 import BeautifulSoup
+
+soup=BeautifulSoup(html_doc,"html.parser")
+
+result=soup.prettify()
+result=soup.title
+result=soup.head
+result=soup.body
+result=soup.title.name
+result=soup.title.string
+result=soup.h1
+result=soup.h1.string
+result=soup.h2
+result=soup.h2.name
+result=soup.h2.string
+result=soup.h2.string[0]
+result=soup.h2.string[1]
+result=soup.div
+result=soup.find_all("div")
+result=soup.find_all("h2")
+result=soup.find_all("h2")[0]
+result=soup.find_all("h2")[1]
+result=soup.div
+result=soup.find_all("div")
+result=soup.find_all("div")[1].ul
+result=soup.find_all("div")[1].ul.li
+result=soup.find_all("div")[1].ul.find_all("li")
+result=soup.div.findChildren()
+result=soup.div.findNextSibling()
+result=soup.div.findNextSibling().findNextSibling()
+result=soup.div.findNextSibling().findNextSibling().findPreviousSibling()
+
+
+result=soup.fin_all("a")
+for link in result:
+    print(link.get("href"))
+    
+
+
+
+
