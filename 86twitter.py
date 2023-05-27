183469415b80e7648c9dd2781aab6855adfca425
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-27 13:23:18 +0300

    commit

--- 86twitter.py
+++ 86twitter.py
@@ -18,30 +18,48 @@ class Twitter:
         
         
     def signIn(self):
-        self.browser.get('https://twitter.com/i/flow/login')
-        self.browser.maximize_window()
-        time.sleep(5)
-        # username input
-        self.browser.find_element(By.CSS_SELECTOR, "input.r-30o5oe.r-1niwhzg.r-17gur6a.r-1yadl64.r-deolkf.r-homxoj.r-poiln3.r-7cikom.r-1ny4l3l.r-t60dpp.r-1dz5y72.r-fdjqy7.r-13qz1uu").send_keys(self.username)
-        # enter
-        self.browser.find_element(By.CSS_SELECTOR, "input.r-30o5oe.r-1niwhzg.r-17gur6a.r-1yadl64.r-deolkf.r-homxoj.r-poiln3.r-7cikom.r-1ny4l3l.r-t60dpp.r-1dz5y72.r-fdjqy7.r-13qz1uu").send_keys(Keys.ENTER)
-        time.sleep(2)
-        # password input
-        self.browser.find_element(By.CSS_SELECTOR, "input.r-30o5oe.r-1niwhzg.r-17gur6a.r-1yadl64.r-deolkf.r-homxoj.r-poiln3.r-7cikom.r-1ny4l3l.r-t60dpp.r-1dz5y72.r-fdjqy7.r-13qz1uu").send_keys(self.password)
-        # enter
-        self.browser.find_element(By.CSS_SELECTOR, "input.r-30o5oe.r-1niwhzg.r-17gur6a.r-1yadl64.r-deolkf.r-homxoj.r-poiln3.r-7cikom.r-1ny4l3l.r-t60dpp.r-1dz5y72.r-fdjqy7.r-13qz1uu").send_keys(Keys.ENTER)
-        time.sleep(20)
+        try:
+            self.browser.get('https://twitter.com/i/flow/login')
+            self.browser.maximize_window()
+            time.sleep(5)
+            # username input
+            self.browser.find_element(By.CSS_SELECTOR, "input.r-30o5oe.r-1niwhzg.r-17gur6a.r-1yadl64.r-deolkf.r-homxoj.r-poiln3.r-7cikom.r-1ny4l3l.r-t60dpp.r-1dz5y72.r-fdjqy7.r-13qz1uu").send_keys(self.username)
+            # enter
+            self.browser.find_element(By.CSS_SELECTOR, "input.r-30o5oe.r-1niwhzg.r-17gur6a.r-1yadl64.r-deolkf.r-homxoj.r-poiln3.r-7cikom.r-1ny4l3l.r-t60dpp.r-1dz5y72.r-fdjqy7.r-13qz1uu").send_keys(Keys.ENTER)
+            time.sleep(2)
+            # password input.r-30o5oer-1niwhzg.r-17gur6a.r-1yadl64.r-deolkf.r-homxoj.r-poiln3.r-7cikom.r-1ny4l3l.r-t60dpp.r-1dz5y72.r-fdjqy7.r-13qz1uu
+            self.browser.find_element(By.XPATH, "//input[@type='password']").send_keys(self.password)
+            # enter
+            self.browser.find_element(By.XPATH, "//input[@type='password']").send_keys(Keys.ENTER)
+            time.sleep(20)
+        except Exception as e:
+            print(e)
 
-
-
+    def search(self,hashtag):
+        try:
+            searchInput=self.browser.find_element(By.CSS_SELECTOR,"input.r-30o5oe.r-1niwhzg.r-17gur6a.r-1yadl64.r-deolkf.r-homxoj.r-poiln3.r-7cikom.r-1ny4l3l.r-xyw6el.r-13rk5gd.r-1dz5y72.r-fdjqy7.r-13qz1uu")
+            searchInput.send_keys(hashtag)
+            time.sleep(2)
+            searchInput.send_keys(Keys.ENTER)
+            time.sleep(2)
+            
+            list=self.browser.find_elements(By.CSS_SELECTOR,"div.css-901oao.r-1nao33i.r-37j5jr.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-bnwqim.r-qvutc0")
+            for item in list:
+                print(item.text)
+            time.sleep(3)
+        except Exception as e:
+            print(e)
 
 
 
 username = ""
 password = ""
-while username == "" and password == "":
+hashtag=""
+while username == "" and password == "" and hashtag=="":
     username = input("username: ")
     password = input("password: ")
+    hashtag = input("search hashtag: ")
 
 twt=Twitter(username,password)
 twt.signIn()
+twt.search(hashtag)
