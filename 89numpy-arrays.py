6308382aa5df97aed10534ea79d34ac7158b1d02
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-29 01:55:19 +0300

    commit

---
+++ 89numpy-arrays.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# result=np.array([1,3,5,7,9])#[1 3 5 7 9]
+# result=np.arange(1,10)#[1 2 3 4 5 6 7 8 9]
+# result=np.arange(10,100,3)#[10 13 16 19 22 25 28 31 34 37 40 43 46 49 52 55 58 61 64 67 70 73 76 79 82 85 88 91 94 97]
+# result=np.zeros(10)#[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+# result=np.ones(10)#[1. 1. 1. 1. 1. 1. 1. 1. 1. 1.]
+# result=np.linspace(0,100,5)#[  0.  25.  50.  75. 100.]
+# result=np.linspace(0,5,5)#[0.   1.25 2.5  3.75 5.  ]
+# result=np.random.randint(0,10)#7
+# result=np.random.randint(20)#8
+# result=np.random.randint(1,10,3)#[3 7 1]
+# result=np.random.rand(5)#[0.20270348 0.17969194 0.7378884  0.32667544 0.1958728 ]
+# result=np.random.randn(5)#[-0.11870452  0.43076922  0.9658678   0.25187098  1.23560892]
+# np_array=np.arange(50)
+# np_multi=np_array.reshape(5,10)
+# print(np_multi.sum(axis=1))#[ 45 145 245 345 445]
+# print(np_multi.sum(axis=0))#[100 105 110 115 120 125 130 135 140 145]
+
+# rnd_numbers=np.random.randint(1,100,10)#[80 39 63 63 39  9 31 18 70 23]
+# print(rnd_numbers)
+      
+# result=rnd_numbers.max()#80
+# result=rnd_numbers.min()#9
+# result=rnd_numbers.mean()#43.5
+# result=rnd_numbers.argmax()#0
+# result=rnd_numbers.argmin()#5
+
+# print(result)
+
