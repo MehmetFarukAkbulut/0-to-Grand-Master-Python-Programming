2cd91c14a9d3c8cfb65a539004bd867807186e7f
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-05-31 02:00:08 +0300

    commit

---
+++ datasets/98imdb.py
@@ -0,0 +1,15 @@
+# 1- Dosyada hakkındaki bilgiler.
+# 2- ilk 5 kaydı gösterin
+# 3- ilk 10 kaydı gösterin
+# 4- Son 5 kaydı gösterin
+# 5- Son 10 kaydı gösterin
+# 6- Sadece Movie_Title kolonunu alın.
+# 7- Sadece Movie_Title kolonunu içeren ilk 5 kaydı alın.
+# 8- Sadece Movie_Title ve Rating kolonunu içeren ilk 5 kaydı alın.
+# 9- Sadece Movie_Title ve Rating kolonunu içeren son 7 kaydı alın.
+# 10- Sadece Movie_Title ve Rating kolonunu içeren ikinci 5 kaydı alın.
+# 11- Sadece Movie_Title ve Rating kolonunu içeren ve imdb puanı 8.0 
+#     ve üstünde olan kayıtlardan ilk 50 tanesini alınız.
+# 12- Yayın tarihi 2014 ile 2015 arasında olan filmlerin isimlerini getiriniz.
+# 13- Değerlendirme sayısı (Num_Reviews) 100.000 den büyük ya da imdb puanı
+#     8 ile 9 arasında olan filmleri listeleyiniz. 
