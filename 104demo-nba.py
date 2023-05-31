ea5a572fba254426312a5161deddfc872a8820db
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-01 00:07:02 +0300

    commit

---
+++ 104demo-nba.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+df= pd.read_csv("datasets/nba.csv")
+result=df
+result=df.columns
+result=df.info
+
+# 1- İlk 10 kaydı getiriniz.
+result=df.head(10)
+# 2- Toplam kaç kayıt vardır ?
+result=len(df.index)
+# 3- Tüm oyuncuların toplam maaş ortalaması nedir ?
+result=df["Salary"].mean()
+# 4- En yüksek maaşı ne kadardır ?
+result=df["Salary"].max()
+
+# 5- En yüksek maaşı alan oyuncu kimdir ?
+result=df[df["Salary"]==df["Salary"].max()]["Name"].iloc[0]
+
+# 6- Yaşı 20-25 arasında olan oyuncuların isim ve oynadıkları takımları azalan şekilde sıralı getiriniz.
+result=df[(df["Age"]>=20)&(df["Age"]<25)][["Name","Team","Age"]].sort_values("Age",ascending=False)
+# 7- "John Holland" isimli oyuncunun oynadığı takım hangisidir ?
+result=df[df["Name"]=="John Holland"]["Team"].iloc[0]
+# 8- Takımlara göre oyuncuların ortalama maaş bilgisi nedir ?
+result=df.groupby("Team")["Salary"].mean()
+# 9- Kaç farklı takım mevcut ?
+result=df["Team"].nunique()
+result=len(df.groupby("Team"))#eşdeğer
+# 10- Her takımda kaç oyuncu oynamaktadır ?
+result=df.groupby("Team")["Name"].count()
+result=df["Team"].value_counts()#eşdeğer
+# 11- İsmi içinde "and" geçen kayıtları bulunuz.
+df.dropna(inplace=True)
+result=df[df["Name"].str.contains("and")]
+
+def str_find(name):
+    if "and" in name.lower():
+        return True
+    return False
+result=df[df["Name"].apply(str_find)]
+
+print(result)
