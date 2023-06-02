d2cc7b56a973f781f53eb2616417eb9d6d7ac7ce
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-02 22:30:07 +0300

    commit

--- 108matplotlib-graphs.py
+++ 108matplotlib-graphs.py
@@ -39,9 +39,35 @@ plt.pie(goals,labels=goal_types,colors=colors, shadow=True,explode=(0.05,0.05,0.
 plt.show()
 
 '''
+'''
+Bar Grafiği
+plt.bar([0.25,1.25,2.25,3.25,4.25],[50,40,70,80,20],label="BMW",width=.5)
+plt.bar([0.75,1.75,2.75,3.75,4.75],[80,20,20,50,60],label="Audi",width=.5)
 
 
+plt.legend()
+plt.xlabel("Gün")
+plt.ylabel("Mesafe(km)")
+plt.title("Araç Bilgileri")
 
+plt.show()
+'''
+'''
+
+Histogram Grafiği
+
+yaslar = [22,55,62,45,21,22,34,42,42,4,2,102,95,85,55,110,120,70,65,55,111,115,80,75,65,54,44,43,42,48]
+yas_gruplari = [0,10,20,30,40,50,60,70,80,90,100]
+
+plt.hist(yaslar,yas_gruplari,histtype="bar",rwidth=0.8)
+
+plt.xlabel("Yaş Grupları")
+plt.ylabel("Kişi Sayısı")
+plt.title("Histogram Grafiği")
+
+plt.show()
+
+'''
 
 
 
