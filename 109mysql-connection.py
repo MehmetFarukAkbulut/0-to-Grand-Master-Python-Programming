fa2876fea8a2fe8ae8ddf8cab885c93fdbfe53cd
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-03 23:33:33 +0300

    commit

--- 109mysql-connection.py
+++ 109mysql-connection.py
@@ -3,12 +3,7 @@ import mysql.connector
 mydb=mysql.connector.connect(
     host="localhost",
     user="root",
-    password="SQL1234"
+    password="SQL1234",
+    database="mydatabase"
 )
 mycursor=mydb.cursor()
-
-mycursor.execute("SHOW DATABASES")
-# mycursor.execute("CREATE DATABASE mydatabase")
-
-for x in mycursor:
-    print(x)
