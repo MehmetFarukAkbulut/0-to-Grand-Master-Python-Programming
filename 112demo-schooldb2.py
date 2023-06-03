a1df9527f1ebdbd49f7c711fcb56a9a3eef50b6a
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-04 01:13:57 +0300

    commit

--- 112demo-schooldb2.py
+++ 112demo-schooldb2.py
@@ -39,20 +39,33 @@ class Student:
             print("Hata: ",err)
         finally:
             Student.connection.close()
+    @staticmethod
+    def saveStudents(students):
+        sql="INSERT INTO Student(StudentNumber,Name,Surname,Birthdate,Gender) VALUES (%s,%s,%s,%s,%s)" 
+        values=students
+        Student.mycursor.executemany(sql,values)
         
-faruk=Student("402","Faruk","Akbulut",datetime(2000,10,23),"E")
-faruk.saveStudent()
+        try:
+            Student.connection.commit()
+            print(f"{Student.mycursor.rowcount} tane kayıt eklendi.")
+        except mysql.connector.Error as err:
+            print("Hata: ",err)
+        finally:
+            Student.connection.close()        
+    
+# faruk=Student("402","Faruk","Akbulut",datetime(2000,10,23),"E")
+# faruk.saveStudent()
 
 
 
 
-# ogrenciler=[    
-#     ("301","Ahmet","Yılmaz",datetime(2005, 5, 17),"E"),
-#     ("302","Ali","Can",datetime(2005, 6, 17),"E"),
-#     ("303","Canan","Tan",datetime(2005, 7, 7),"K"),
-#     ("304","Ayşe","Taner",datetime(2005, 9, 23),"K"),
-#     ("305","Bahadır","Toksöz",datetime(2004, 7, 27),"E"),
-#     ("306","Ali","Cenk",datetime(2003, 8, 25),"E")
-# ]
-# mycursor.executemany(sql,ogrenciler)
+students=[    
+    ("501","Cem","Yılmaz",datetime(2005, 5, 17),"E"),
+    ("502","Fatih","Akbulut",datetime(2005, 6, 17),"E"),
+    ("503","Rümeysa","Bal",datetime(2005, 7, 7),"K"),
+    ("504","Ayşe","Şener",datetime(2005, 9, 23),"K"),
+    ("505","Yılmaz","Vural",datetime(2004, 7, 27),"E"),
+    ("506","Arda","Turan",datetime(2003, 8, 25),"E")
+]
 
+Student.saveStudents(students)
