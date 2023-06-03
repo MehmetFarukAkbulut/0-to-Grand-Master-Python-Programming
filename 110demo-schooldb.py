fbbbe688fecf7725da66acf20f0baaefd7220fcd
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-03 23:35:26 +0300

    commit

---
+++ 110demo-schooldb.py
@@ -0,0 +1,4 @@
+# 1- Workbench IDE ile schooldb isminde bir database oluşturup Student tablosunu ekleyiniz.
+    # Id,StudentNumber,Name,Surname,Birthdate,Gender
+
+# 2- Database bağlantısını oluşturunuz. (connection.py)
