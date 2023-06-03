1d9689d1eff8878b4064e0d8fc36576bc84ab60b
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-04 00:11:16 +0300

    commit

---
+++ 111insert.py
@@ -0,0 +1,58 @@
+import mysql.connector
+
+
+def insertProduct(name,price,imageUrl,description):
+    connection=mysql.connector.connect(host="localhost",user="root",password="SQL1234",database="sys")
+    cursor=connection.cursor()
+
+    sql="INSERT INTO Products(name,price,imageUrl,description) VALUES (%s,%s,%s,%s)"
+    values=(name,price,imageUrl,description)
+    
+    
+    cursor.execute(sql,values)
+    try:    
+        connection.commit()
+        print(f"{cursor.rowcount} tane kayıt eklendi")
+        print(f"son eklenen kaydın id:  {cursor.lastrowid}")
+    except mysql.connector.Error as err:
+        print("Hata: ",err)
+    finally:
+        connection.close()
+        print("database bağlantısı kapandı")
+        
+def insertProducts(list):
+    connection=mysql.connector.connect(host="localhost",user="root",password="SQL1234",database="sys")
+    cursor=connection.cursor()
+
+    sql="INSERT INTO Products(name,price,imageUrl,description) VALUES (%s,%s,%s,%s)"
+    values=list
+    
+    
+    cursor.executemany(sql,values)
+    try:    
+        connection.commit()
+        print(f"{cursor.rowcount} tane kayıt eklendi")
+        print(f"son eklenen kaydın id:  {cursor.lastrowid}")
+    except mysql.connector.Error as err:
+        print("Hata: ",err)
+    finally:
+        connection.close()
+        print("database bağlantısı kapandı")
+
+list=[]
+while True:
+    name=input("ürün adı: ")
+    price=float(input("ürün fiyatı: "))
+    imageUrl=input("ürün resim adı: ")
+    description=input("ürün açıklaması: ")
+
+    list.append((name,price,imageUrl,description))
+    result=input("devam etmek istiyor musunuz? (e/h): ")
+    if result=="h":
+        print("Kayıtlarınız veritabanına aktarılıyor...")
+        print(list)
+        insertProducts(list)
+        break
+        #kayıt    
+
+# insertProduct(name,price,imageUrl,description)
