f8200f7375787bfe9fb256aeac31ae1e22c2ff99
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-03 23:42:30 +0300

    commit

--- 110demo-schooldb.py
+++ 110demo-schooldb.py
@@ -2,3 +2,15 @@
     # Id,StudentNumber,Name,Surname,Birthdate,Gender
 
 # 2- Database bağlantısını oluşturunuz. (connection.py)
+
+import mysql.connector
+
+connection=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="SQL1234",
+    database="schooldb"
+)
+
+mycursor=connection.cursor()
+
