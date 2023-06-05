348cf76d3b319d3ad3b6cb7b0616cf7712f4feee
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-05 22:56:58 +0300

    commit

--- 114filters.py
+++ 114filters.py
@@ -46,7 +46,8 @@ def getProducts():
     # cursor.execute("Select * From Products Where id=1")
     # cursor.execute("Select * From Products Where name='Samsung S8'")
     # cursor.execute("Select * From Products Where name='Samsung S8' and price>=3000 ")
-    cursor.execute("Select * From Products Where name='Samsung S8' or price>=3000 ")
+    # cursor.execute("Select * From Products Where name='Samsung S8' or price>=3000 ")
+    cursor.execute("Select * From Products Where name LIKE '%Samsung%' ")
     
     
     result=cursor.fetchall()
