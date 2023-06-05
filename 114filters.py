138b799e5dddeeb48ee7e456c5f2038417f16143
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-05 22:44:42 +0300

    commit

---
+++ 114filters.py
@@ -0,0 +1,56 @@
+import mysql.connector
+
+
+def insertProduct(name,price,imageUrl,description):
+    connection=mysql.connector.connect(host="localhost",user="root",password="SQL1234",database="sys")
+    cursor=connection.cursor()
+
+    sql="INSERT INTO Products(name,price,imageUrl,description) VALUES (%s,%s,%s,%s)"
+    values=(name,price,imageUrl,description)
+    
+    
+    cursor.execute(sql,values)
+    try:    
+        connection.commit()
+        print(f"{cursor.rowcount} tane kayıt eklendi")
+        print(f"son eklenen kaydın id:  {cursor.lastrowid}")
+    except mysql.connector.Error as err:
+        print("Hata: ",err)
+    finally:
+        connection.close()
+        print("database bağlantısı kapandı")
+        
+def insertProducts(list):
+    connection=mysql.connector.connect(host="localhost",user="root",password="SQL1234",database="sys")
+    cursor=connection.cursor()
+
+    sql="INSERT INTO Products(name,price,imageUrl,description) VALUES (%s,%s,%s,%s)"
+    values=list
+    
+    
+    cursor.executemany(sql,values)
+    try:    
+        connection.commit()
+        print(f"{cursor.rowcount} tane kayıt eklendi")
+        print(f"son eklenen kaydın id:  {cursor.lastrowid}")
+    except mysql.connector.Error as err:
+        print("Hata: ",err)
+    finally:
+        connection.close()
+        print("database bağlantısı kapandı")
+
+def getProducts():
+    connection=mysql.connector.connect(host="localhost",user="root",password="SQL1234",database="sys")
+    cursor=connection.cursor()
+    
+    # cursor.execute("Select * From Products Where id=1")
+    cursor.execute("Select * From Products Where name='Samsung S8'")
+    
+    
+    result=cursor.fetchall()
+    
+    for product in result:
+        # print(f"name: {product[1]} price: {product[2]}")
+        print(f"id: {product[0]} name: {product[1]} price: {product[2]}")
+    
+getProducts()
