ba703732e2c7dcff07990baeec68c84f4cd426d1
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-09 00:34:16 +0300

    commit

--- 121schoolapp/dbmanager.py
+++ 121schoolapp/dbmanager.py
@@ -5,10 +5,21 @@ from Student import Student
 from Teacher import Teacher
 
 class DbManager:
-    def __init__(self,connection):
+    def __init__(self):
         self.connection=connection
         self.cursor=self.connection.cursor()
     
+    def getStudentsById(self,id):
+        sql="select * from student where id=%s"
+        value=(id,)
+        self.cursor.execute(sql,value)
+        try:
+            obj=self.cursor.fetchone()
+            return Student(obj[0],obj[1],obj[2],obj[3],obj[4],obj[5],obj[6],)
+        except mysql.connector.Error as err:
+            print("Error: ",err)
+    def getStudentsByClassId(self,id):
+        pass
     def addStudent(self,student: Student):
         pass
 
@@ -21,4 +32,7 @@ class DbManager:
     def editTeacher(self,teacher: Teacher):
         pass
         
-        
+db=DbManager()
+student=db.getStudentsById(7)
+print(student.name)   
+print(student.surname)   
