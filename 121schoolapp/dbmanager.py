8e0b4d958ffdcaf675fdb9198f0729f25ddeda7d
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-09 00:37:32 +0300

    commit

--- 121schoolapp/dbmanager.py
+++ 121schoolapp/dbmanager.py
@@ -18,8 +18,16 @@ class DbManager:
             return Student(obj[0],obj[1],obj[2],obj[3],obj[4],obj[5],obj[6],)
         except mysql.connector.Error as err:
             print("Error: ",err)
-    def getStudentsByClassId(self,id):
-        pass
+    def getStudentsByClassId(self,classid):
+        sql="select * from student where classid=%s"
+        value=(classid,)
+        self.cursor.execute(sql,value)
+        try:
+            obj=self.cursor.fetchall()
+            print(obj)
+            # return Student(obj[0],obj[1],obj[2],obj[3],obj[4],obj[5],obj[6],)
+        except mysql.connector.Error as err:
+            print("Error: ",err)
     def addStudent(self,student: Student):
         pass
 
@@ -33,6 +41,8 @@ class DbManager:
         pass
         
 db=DbManager()
-student=db.getStudentsById(7)
-print(student.name)   
-print(student.surname)   
+# student=db.getStudentsById(7)
+# print(student.name)   
+# print(student.surname)   
+
+students=db.getStudentsByClassId(1)
