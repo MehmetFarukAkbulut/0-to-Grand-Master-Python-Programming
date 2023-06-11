bacd30edaf6c7a907e3a00e88ad94f93dffce9a7
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-11 22:14:49 +0300

    commit

--- 130win-items.py
+++ 130win-items.py
@@ -3,41 +3,52 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication,QMainWindow,QToolTip
 from PyQt5.QtGui import QIcon
 
-
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow,self).__init__()
+            
+        self.setWindowTitle("First Application")
+        self.setGeometry(200,200,700,700)
+        self.setWindowIcon(QIcon("icon.png"))
+        self.setToolTip("My Tooltip")
+        self.initUI()
+    def initUI(self):
+        self.lbl_name=QtWidgets.QLabel(self)
+        self.lbl_name.setText("Adınız: ")
+        self.lbl_name.move(50,30)
+        
+        self.lbl_surname=QtWidgets.QLabel(self)
+        self.lbl_surname.setText("Soyadınız: ")
+        self.lbl_surname.move(50,70)
+        
+        self.lbl_result=QtWidgets.QLabel(self)
+        self.lbl_result.resize(300,50) 
+        self.lbl_result.move(150,150)
+        self.lbl_result.resize(200,32)
+        
+        
+        self.txt_name=QtWidgets.QLineEdit(self)
+        self.txt_name.move(150,30)
+        self.txt_name.resize(200,32)
+        
+        self.txt_surname=QtWidgets.QLineEdit(self)
+        self.txt_surname.move(150,70)
+        self.txt_surname.resize(200,32)
+        
+        
+        self.btn_save=QtWidgets.QPushButton(self)
+        self.btn_save.setText("Kaydet")
+        self.btn_save.move(150,110)
+        self.btn_save.clicked.connect(self.clicked)
+    def clicked(self):
+        self.lbl_result.setText("name: "+ self.txt_name.text()+ "\nsurname: "+ self.txt_surname.text())
+        
+    
+    
+    
 def window():
     app=QApplication(sys.argv)
-    win=QMainWindow()
-    
-    
-    win.setWindowTitle("First Application")
-    win.setGeometry(200,200,700,700)
-    win.setWindowIcon(QIcon("icon.png"))
-    win.setToolTip("My Tooltip")
-    
-    lbl_name=QtWidgets.QLabel(win)
-    lbl_name.setText("Adınız: ")
-    lbl_name.move(50,30)
-    
-    lbl_surname=QtWidgets.QLabel(win)
-    lbl_surname.setText("Soyadınız: ")
-    lbl_surname.move(50,70)
-    
-    txt_name=QtWidgets.QLineEdit(win)
-    txt_name.move(150,30)
-    
-    txt_surname=QtWidgets.QLineEdit(win)
-    txt_surname.move(150,70)
-    
-    def clicked(self):
-        print("Butona Tıklandı \nname: "+ txt_name.text()+ "\nsurname: "+ txt_surname.text())
-    
-    
-    btn_save=QtWidgets.QPushButton(win)
-    btn_save.setText("Kaydet")
-    btn_save.move(150,110)
-    btn_save.clicked.connect(clicked)
-    
-    
+    win=MyWindow()
     win.show()
     sys.exit(app.exec_())
     
