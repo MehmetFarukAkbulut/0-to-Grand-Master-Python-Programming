4e7985d6228d23764ccaa56e59e04c71084665e4
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-15 00:12:25 +0300

    commit

--- 133checkbox.py
+++ 133checkbox.py
@@ -7,13 +7,32 @@ class myApp(QtWidgets.QMainWindow):
         super(myApp,self).__init__()
         
         self.ui=Ui_MainWindow()
+        self.setGeometry(200,200,500,500)
+        
         self.ui.setupUi(self)
             
         self.ui.cbSinema.stateChanged.connect(self.show_state)
         self.ui.cbKitap.stateChanged.connect(self.show_state)
         self.ui.cbSpor.stateChanged.connect(self.show_state)
         
+        self.ui.btnHobilerSecilenleriAl.clicked.connect(self.getAllHobiler)
+        self.ui.btnDerslerSecilenleriAl.clicked.connect(self.getAllDersler)
         
+    def getAllHobiler(self):
+        result=''
+        items=self.ui.groupHobiler.findChildren(QtWidgets.QCheckBox)
+        for cb in items:
+            if cb.isChecked():
+                result+= cb.text()+'\n'
+        self.ui.lblResultHobiler.setText(result)
+    def getAllDersler(self):
+        result=''
+        items=self.ui.groupDersler.findChildren(QtWidgets.QCheckBox)
+        for cb in items:
+            if cb.isChecked():
+                result+= cb.text()+'\n'
+        self.ui.lblResultDersler.setText(result)
+    
     def show_state(self,value):
         cb=self.sender()
         print(value)
