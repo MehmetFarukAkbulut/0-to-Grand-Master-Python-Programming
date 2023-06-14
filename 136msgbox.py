a5e6ae26dd0fab553fea176f208d3a56b340133c
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-15 01:41:57 +0300

    commit

---
+++ 136msgbox.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5 import QtWidgets
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Window,self).__init__()
+        
+        # self.ui=Ui_MainWindow()
+        # self.setGeometry(200,200,500,500)
+        
+        # self.ui.setupUi(self)
+
+app=QtWidgets.QApplication(sys.argv)
+win=Window()
+win.show()
+sys.exit(app.exec_())
