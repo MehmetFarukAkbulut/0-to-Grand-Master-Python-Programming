cc161882da3d63413eb3dbb36df9ca5b34bd47a5
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-17 02:41:47 +0300

    commit

--- 137date.py
+++ 137date.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import QtWidgets
 from dateForm import Ui_MainWindow
+from PyQt5.QtCore import QDate,QTime,QDateTime
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
@@ -10,6 +11,19 @@ class Window(QtWidgets.QMainWindow):
         self.setGeometry(200,200,500,500)
         
         self.ui.setupUi(self)
+        self.ui.btnCalculate.clicked.connect(self.calculate)
+        
+    def calculate(self):
+        start=self.ui.dateStart.date()
+        end=self.ui.dateEnd.date()
+        print(start, end)
+        print(f'Days in month: {start.daysInMonth()}')
+        print(f'Days in year: {start.daysInYear()}')
+        print(f'Total days: {start.daysTo(end)}')
+        
+        now= QDate.currentDate()
+        
+        print(f'total days from now: {start.daysTo(now)}')
 
 
 app=QtWidgets.QApplication(sys.argv)
