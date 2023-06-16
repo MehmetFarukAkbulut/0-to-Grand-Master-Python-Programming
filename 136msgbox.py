d0c163df58569752e451f585f878e5f47a1f796d
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-16 22:53:19 +0300

    commit

--- 136msgbox.py
+++ 136msgbox.py
@@ -1,15 +1,34 @@
 import sys
 from PyQt5 import QtWidgets
+from msgboxForm import Ui_MainWindow
+from PyQt5.QtWidgets import QMessageBox
+
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super(Window,self).__init__()
         
-        # self.ui=Ui_MainWindow()
-        # self.setGeometry(200,200,500,500)
+        self.ui=Ui_MainWindow()
+        self.setGeometry(200,200,500,500)
         
-        # self.ui.setupUi(self)
+        self.ui.setupUi(self)
+        
+        self.ui.btnExit.clicked.connect(self.showDialog)
 
+    def showDialog(self):
+        msg=QMessageBox()
+        msg.setWindowTitle('Close Application')
+        msg.setGeometry(450,450,500,500)
+        
+        msg.setText('Are you sure?')
+        # msg.setIcon(QMessageBox.Question)
+        msg.setIcon(QMessageBox.Warning)
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Ignore | QMessageBox.Cancel)
+        msg.setDefaultButton(QMessageBox.Ignore)
+        x=msg.exec_()
+        
+    
+    
 app=QtWidgets.QApplication(sys.argv)
 win=Window()
 win.show()
