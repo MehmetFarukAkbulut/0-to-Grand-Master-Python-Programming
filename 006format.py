eee4147840289aef1acab4ac32ee6d8cd55b4858
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-06-29 12:29:14 +0300

    Update 006format.py

--- 006format.py
+++ 006format.py
@@ -12,4 +12,4 @@ age= 23
 # print("The result is {r:1.3}".format(r=result))
 # print("The result is {r:10.4}".format(r=result))
 # print("The result is {r:7.4}".format(r=result))
-print(f"My name is {name} {surname} and I'm {age} years old.")
+print(f"My name is {name} {surname}, and I'm {age} years old.")
