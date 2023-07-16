27b8ff0d6e02aa3be074ff5d2c9fef1fb99e11f0
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-16 21:07:35 +0300

    commit

--- catalog/movies/models.py
+++ catalog/movies/models.py
@@ -7,7 +7,7 @@ class Movie(models.Model):
     description=models.TextField(verbose_name='Film Açıklaması')
     image=models.CharField(max_length=50,verbose_name='Film Resmi')
     created_date=models.DateTimeField(auto_now_add=True, verbose_name='Eklenme Tarihi ')
-    
+    isPublished=models.BooleanField(default=True)
     
     def __str__(self):
         return self.name
