c289314f7be94c7b6fc6fb50eeb11c6c54bf5ca0
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-16 20:54:22 +0300

    commit

--- catalog/movies/admin.py
+++ catalog/movies/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Movie
 
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display= ('id','name','created_date')
+
+
 # Register your models here.
 
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
