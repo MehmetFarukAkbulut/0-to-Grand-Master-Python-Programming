c58f005e66099a92d53832802225975a3ae63ddc
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-16 20:51:56 +0300

    commit

--- catalog/movies/models.py
+++ catalog/movies/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    name=models.CharField(max_length=100)
-    description=models.TextField()
-    image=models.CharField(max_length=50)
-    created_date=models.DateTimeField(auto_now_add=True)
+    name=models.CharField(max_length=100,verbose_name='Film Adı')
+    description=models.TextField(verbose_name='Film Açıklaması')
+    image=models.CharField(max_length=50,verbose_name='Film Resmi')
+    created_date=models.DateTimeField(auto_now_add=True, verbose_name='Eklenme Tarihi ')
     
     
     def __str__(self):
