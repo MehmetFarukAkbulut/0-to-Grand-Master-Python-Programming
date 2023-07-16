4e79d42ae6454f3142779448c633eb3467ab93b6
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-16 21:17:07 +0300

    commit

--- catalog/movies/admin.py
+++ catalog/movies/admin.py
@@ -3,9 +3,11 @@ from .models import Movie
 
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display= ('id','name','created_date')
+    list_display= ('id','name','created_date','isPublished')
     list_display_links= ('id','name')
     list_filter= ('created_date',)
+    list_editable= ('isPublished',)
+    
 
 
 # Register your models here.
