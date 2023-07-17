9b11be370219dbf864aec13ea0732a7f2aa00110
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-17 23:03:56 +0300

    commit

--- catalog/movies/views.py
+++ catalog/movies/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'movies/list.html')
+    context={
+        'name': 'Mehmet Faruk Akbulut'
+    }
+    return render(request, 'movies/list.html', context)
+
 def detail(request):
     return render(request, 'movies/detail.html')
 def search(request):
