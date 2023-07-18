0ab3a36b2a02eb83e8316d5af3055d79fcc4c66a
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-18 22:47:00 +0300

    commit

--- catalog/user/views.py
+++ catalog/user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -9,8 +10,32 @@ def login(request):
 
 def register(request):
     if request.method=='POST':
-        print('submitted')
-        return redirect('register')
+        #get form values
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        repassword=request.POST['repassword']
+        
+        if password==repassword:
+            #Username
+            if User.objects.filter(username=username).exists():
+                print('Bu kullanıcı adı daha önce alınmış.')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    print('Bu email daha önce kullanılmış.')
+                    return redirect('register')
+                else:
+                    #her şey güzel
+                    user=User.objects.create_user(username=username,password=password,email=email)
+                    user.save()
+                    print('Kullanıcı oluşturuldu.')
+                    return register('login')
+            
+            
+        else:
+            print('Parolalar eşleşmiyor.')
+            return redirect('register')
     else:
         return render(request,'user/register.html')
 
