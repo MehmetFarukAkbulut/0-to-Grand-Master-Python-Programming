7f8b911224b0996d5f3e6cc5ae0c528fb232d3e9
Mehmet Faruk Akbulut <78991543+MehmetFarukAkbulut@users.noreply.github.com>
2023-07-18 20:02:11 +0300

    commit

--- catalog/movies/views.py
+++ catalog/movies/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 from .models import Movie
 # Create your views here.
 
@@ -10,6 +12,12 @@ def index(request):
     return render(request, 'movies/list.html', context)
 
 def detail(request,movie_id):
-    return render(request, 'movies/detail.html')
+
+    movie= get_object_or_404(Movie,pk=movie_id)
+    context={
+        'movie':movie
+    }
+    return render(request, 'movies/detail.html',context)
+
 def search(request):
     return render(request, 'movies/search.html')
